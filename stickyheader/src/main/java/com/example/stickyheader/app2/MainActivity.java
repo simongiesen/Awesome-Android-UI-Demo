3dcf0717417696aa3b89f0b75a9eb9e177921b7a
Ichen.tsu <r99521320@gmail.com>
2016-12-26 18:42:35 +0800

    Listview-sticky-header

---
+++ stickyheader/src/main/java/com/example/stickyheader/app2/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.stickyheader.app2;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            StickyListHeadersListView stickyList = (StickyListHeadersListView) rootView.findViewById(R.id.listContainer);
+            MyAdapter adapter = new MyAdapter(getContext());
+            stickyList.setAdapter(adapter);
+            return rootView;
+        }
+    }
+}
